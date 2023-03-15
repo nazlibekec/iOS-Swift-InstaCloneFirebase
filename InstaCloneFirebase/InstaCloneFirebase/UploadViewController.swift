a652947088bb61b756aaea6c0572be17da0f7cc1
nazlibekec <nazlibekec@gmail.com>
2023-03-15 17:53:47 +0300

    Storage'a resim yükleme işlemleri yapıldı.

--- InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -16,11 +18,9 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        
         imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
         imageView.addGestureRecognizer(gestureRecognizer)
-
     }
     
     @objc func chooseImage() {
@@ -32,10 +32,41 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
+    func makeAlert (titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediafolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            //her bir resmi farklı isimde kaydetme
+            let uuid = UUID().uuidString
+            let imageReference = mediafolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata")
+                } else {
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                           // print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
 }
